a977c0f74b9d8fbb9c9a2a174ed61c7820b1915f
timthedev07 <timpersonal07@gmail.com>
2024-06-02 11:23:38 +0200

    fix please

--- src/app/blog/(blogs)/layout.tsx
+++ src/app/blog/(blogs)/layout.tsx
@@ -4,29 +4,20 @@ import { headers } from "next/headers";
 import { join } from "path";
 import { readFile, readdir } from "fs/promises";
 import { extractHeadings } from "../../../lib/extractHeadings";
-import { Metadata, ResolvingMetadata } from "next";
-import { getMetadata } from "../../../lib/seo";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
 
 const Layout: FC<LayoutProps> = async ({ children }) => {
   const headersList = headers();
   const pathname = headersList.get("x-pathname") || "";
   const slug = pathname.split("/").slice(2);
-  console.log(await readdir(join(process.cwd(), ".next")));
   const fpath = join(
     process.cwd(),
-    ...(process.env.NODE_ENV === "development" ? [] : [".next"]),
-    "src",
-    "app",
-    "blog",
-    "(blogs)",
+    ...(process.env.NODE_ENV === "development"
+      ? ["src", "app", "blog", "(blogs)"]
+      : ["aux"]),
     ...slug,
     "page.mdx"
   );
