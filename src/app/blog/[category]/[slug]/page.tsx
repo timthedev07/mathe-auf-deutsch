88eb0445fd453d8682fbf82c299443e598d6f8f9
timthedev07 <timpersonal07@gmail.com>
2026-07-13 20:27:03 +0800

    update

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -19,7 +19,9 @@ type Props = {
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
   const fpath = join(process.cwd(), "blogs");
-  const categories = await readdir(fpath);
+  const categories = (await readdir(fpath)).filter(
+    (a) => a.search(/\./) === -1,
+  );
   const identifiers: Props["params"][] = [];
 
   for (const category of categories) {
@@ -54,13 +56,13 @@ const Page: FC<Props> = async ({ params: { category, slug } }) => {
       <BlogAside headings={(headings as any)[`${category}/${slug}`]} />
       <main className="fixed flex w-full top-0 lg:w-[calc(100%-20rem)] flex-col gap-4 h-full overflow-y-auto lg:left-80 p-24 pb-64">
         <header className="relative md:w-7/10 w-8/10 mt-12">
-          <h1 className="relative font-bold break-words max-w-[80%] text-white text-4xl border-b-4 pb-1 border-b-cyan-400/60 transition duration-200 hover:border-b-cyan-400/80 text-center w-max mx-auto">
+          <h1 className="relative font-bold break-words max-w-[80%] text-white text-4xl underline decoration-cyan-400/60 pb-1 transition duration-200 hover:border-b-cyan-400/80 text-center w-max mx-auto">
             {frontmatter.title}
           </h1>
           <h2 className="text-white/80 text-lg text-center mt-6">
             <CalendarOutlined />{" "}
             {Intl.DateTimeFormat("de-DE", { dateStyle: "full" }).format(
-              new Date(frontmatter.date)
+              new Date(frontmatter.date),
             )}
           </h2>
           <ul className="w-full flex justify-center gap-4 my-6">
